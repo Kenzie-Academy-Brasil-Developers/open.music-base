cf559a750f93f9c83970bf05237545fc08275e6c
Joao Borchoski <joaohenriqueborchoski>
2022-10-05 13:58:54 -0300

    entrega

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,105 @@
-/* Desenvolva sua lógica aqui ... */
+
+const ulMainCards = document.getElementById("ulMainCards")
+const valorInput = document.getElementById("valorInput")
+const valorOutput = document.getElementById("valorOutput")
+
+
+function criaCards(arr){
+    ulMainCards.innerHTML = ""
+    arr.forEach(element => {
+        let li = document.createElement("li")
+        let figure = document.createElement("figure")
+        let img = document.createElement("img")
+        let sectionPrincipal = document.createElement("section")
+        let section2 = document.createElement("section")
+        let album = document.createElement("p")
+        let ano = document.createElement("p")
+        let section3 = document.createElement("section")
+        let band = document.createElement("h4")
+        let section4 = document.createElement("section")
+        let valor = document.createElement("h4")
+        let button = document.createElement("button")
+
+        img.src = element.img
+        album.innerText = element.title
+        ano.innerText = element.year
+        band.innerText = element.band
+        valor.innerText = `R$ ${element.price},00`
+        button.innerText = "Comprar"
+
+        li.classList.add("card")
+        sectionPrincipal.classList.add("descriptionCard")
+        section2.classList.add("flex")
+        section2.classList.add("align")
+        section2.classList.add("gap16")
+        section4.classList.add("flex")
+        section4.classList.add("align")
+        section4.classList.add("justifyBet")
+        section4.classList.add("widthFull")
+        button.classList.add("but")
+
+        figure.appendChild(img)
+        section2.append(album, ano)
+        section3.appendChild(band)
+        section4.append(valor, button)
+        sectionPrincipal.append(section2, section3, section4)
+        li.append(figure, sectionPrincipal)
+        ulMainCards.appendChild(li)
+    });
+}
+criaCards(products)
+
+function alteraValor(){
+    valorInput.addEventListener("input", () => {
+        valorOutput.innerHTML = `${valorInput.value},00`
+    });
+}
+alteraValor()
+
+// function valorInput2(){
+//     return valorInput.value
+// }
+
+
+function valorFilter() {
+    valorInput.addEventListener("input", () => {
+        const filteredByValue = products.filter(element => element.price <= valorInput.value)
+
+        criaCards(filteredByValue)
+    });
+}
+valorFilter()
+
+function buttonsFilter() {
+    const filterButtons = document.querySelectorAll(".butEstilos")
+
+    filterButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            ulMainCards.innerHTML = ""
+            const filter = button.innerText
+            
+            
+            if(filter === "Todos") {
+                criaCards(products)
+            }
+            else{
+                const fiteredSongs = filterSong(filter)
+                criaCards(fiteredSongs)
+            }
+        })
+    })
+}
+function filterSong(text){
+    const SongsFiltered = products.filter(song => song.category === text)
+    
+    return SongsFiltered
+}
+buttonsFilter()
+
+
+
+
+
+
+
+
--- scripts/theme.js
+++ scripts/theme.js
@@ -1,1 +1,41 @@
-/* Desenvolva sua lógica aqui ... */
+function dmferify(){
+    const dmPref = localStorage.getItem("darkmode")
+    const html = document.querySelector("html")
+    const dmIcon = document.getElementById("dmIcon")
+    
+    if(dmPref){
+        html.classList.add("dark-mode")
+        dmIcon.src = "./assets/img/sun.svg"
+        
+    }
+    if(!dmPref){
+        dmIcon.src = "./assets/img/moon.svg"
+        
+    }    
+}
+dmferify()
+
+
+
+function darkMode() {
+    const dmButton = document.getElementById("dmButton")
+    const html = document.querySelector("html")
+    const dmIcon = document.getElementById("dmIcon")
+    
+
+    dmButton.addEventListener("click", () => {
+        html.classList.toggle("dark-mode")
+    
+        const dmPref = localStorage.getItem("darkmode")
+
+        if(!dmPref){
+            localStorage.setItem("darkmode", true)
+            dmIcon.src = "./assets/img/sun.svg"
+        }
+        if(dmPref){
+            localStorage.removeItem("darkmode")
+            dmIcon.src = "./assets/img/moon.svg"
+        }
+    })
+}
+darkMode()
